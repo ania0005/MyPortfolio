95d891342dd85d9c22724612d1c59a6a0a0c282a
Таня Самойленко <ania000535@gmail.com>
2024-06-10 17:53:39 +0200

    add

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <ScrollToTop />
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/MyPortfolio" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="/contacts" element={<Contacts />} />
