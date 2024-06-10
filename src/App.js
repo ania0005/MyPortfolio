75ff873062988f3b5379f013b267a0212cc9a524
Таня Самойленко <ania000535@gmail.com>
2024-06-10 22:42:31 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./styles/main.css";
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 import Project from "./pages/Project";
-
 import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
